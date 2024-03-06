ac14c6c45be07fd6a7374456d13619456e50f96d
Sasha Yatsykovych <olexandr.y@mate.academy>
2024-03-06 11:52:51 +0200

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 
-// import usersFromServer from './api/users';
-// import productsFromServer from './api/products';
-// import categoriesFromServer from './api/categories';
+import categoriesFromServer from "./api/categories";
+import { CategoryCard } from "./components/CategoryCard";
 
 export const App = () => (
   <div className="container">
-    <div className="ui card">
-      <div className="ui content">
-        <div className="ui description">
-          <p>Grocery - (Anna)</p>
-
-          <ul className="ui list">
-            <li>Bread</li>
-            <li>Eggs</li>
-            <li>Sugar</li>
-          </ul>
-        </div>
-      </div>
-    </div>
-
-    <div className="ui card">
-      <div className="ui content">
-        <div className="ui description">
-          <p>Electronics - (Roma)</p>
-          <b>No products</b>
-        </div>
-      </div>
-    </div>
+    {categoriesFromServer.map((category) => (
+      <CategoryCard key={category.id} category={category} />
+    ))}
   </div>
 );
